2da5cd7cdf3e819968310a849ea7cf01a12c2c65
tsherlock86 <tadsherlock@gmail.com>
2015-07-22 23:42:04 -0400

    Added Form Tag

---
+++ Bootstrapping-Rails/app/models/lipsum.rb
@@ -0,0 +1,22 @@
+class Lipsum
+
+  def self.lorem(value)
+
+    {
+
+    :standard =>  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gsalley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+
+
+    :hipster => "Sustainable next level vinyl bicycle rights Carles cliche, Shoreditch health goth Helvetica aesthetic swag listicle kogi. Twee fap single-origin coffee street art. Wolf mixtape try-hard slow-carb, migas scenester Wes Anderson drinking vinegar authentic kitsch quinoa. Direct trade Williamsburg sriracha, selfies four loko drinking vinegar beard 3 wolf moon Banksy Blue Bottle leggings butcher wolf tote bag Shoreditch. Shabby chic butcher quinoa, slow-carb PBR narwhal cold-pressed banhs mi McSweeney's. Vinyl synth Echo Park wayfarers crucifix, asymmetrical bicycle rights Vice tofu +1 church-key. Four loko brunch occupy, pug swag before they sold out Echo Park keytar semiotics sustainable 8-bit.",
+
+
+
+    :roman => "Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, bibendum eu pulvinar vel, sodales vitae dui. Pellentesque sed sapien lorem, at lacinia urna. In hac habitasse platea dictumst. Vivamus vel justo in leo laoreet ullamcorper non vitae lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ullamcorper rutrum."
+
+  }
+
+  end
+
+
+end
